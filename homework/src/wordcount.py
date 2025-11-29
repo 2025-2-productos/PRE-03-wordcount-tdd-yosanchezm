dc15468a4b4b2badaa957341091d4aaf339e0e51
Camilo Sánchez <yosanchezm@unal.edu.co>
2025-11-29 11:53:57 -0500

    Carga funciones y prueba de testeo

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,1 +1,27 @@
+# Ejemplo para casos de uso
+# python3 -m homework data/input data/output
 
+
+import argparse
+
+from homework.src._internals.read_all_lines import read_all_lines
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Count Word in files.")
+    parser.add_argument(
+        "input", type=str, help="Path to the input folder containing files to process."
+    )
+    parser.add_argument(
+        "output",
+        type=str,
+        help="Path to the output folder containing files to process.",
+    )
+
+    parsed_args = parser.parse_args()
+
+    return parsed_args.input, parsed_args.output
+
+def main():
+    input_folder, output_folder = parse_args()
+    line = read_all_lines(input_folder)
